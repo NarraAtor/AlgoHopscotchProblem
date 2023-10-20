9a9933a6575376126a45d048c5c94857840e5b30
NarraAtor <eff3587@rit.edu>
2023-10-20 17:14:49 -0400

    Created Hopscotch.java

---
+++ src/Hopscotch.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Hopscotch {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int sizeOfData = scanner.nextInt();
+        int[] hopscotchArray = new int[sizeOfData];
+
+        for(int i = 0; i < sizeOfData; i++)
+        {
+            hopscotchArray[i] = scanner.nextInt();
+        }
+
+//        for(int i =0; i < sizeOfData; i++)
+//        {
+//            System.out.println(hopscotchArray[i]);
+//        }
+
+        int[] coolerHopscotchArray = new int[sizeOfData - 2];
+        for(int i = 2; i < sizeOfData; i++)
+        {
+            coolerHopscotchArray[i - 2] = hopscotchArray[i];
+        }
+
+        int[] S = new int[sizeOfData - 2];
+
+        for(int j = 0; j < sizeOfData - 2; j++)
+        {
+            if (j <= 1)
+            {
+                S[j] = coolerHopscotchArray[j];
+            } else if (j == 3) {
+                S[j] = coolerHopscotchArray[j] + S[j - 2];
+            }
+            else if (j > 3) {
+                S[j] = Math.max(coolerHopscotchArray[j] + S[j-2], coolerHopscotchArray[j] + S[j - 3]);
+            }
+        }
+        scanner.close();
+    }
+
+}
